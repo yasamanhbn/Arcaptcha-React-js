c8441a6e97c76785db31e9a79b1a0ce987fe9e71
yasamanhbn <hbn.yasaman@gmail.com>
2022-05-21 19:40:36 +0430

    simplizes render

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
-import { ArcaptchaWidget } from './lib/index'
-class App extends Component{
-  constructor(){
+import React, { Component } from "react";
+import { ArcaptchaWidget } from "./lib/index";
+class App extends Component {
+  constructor() {
     super();
     this.ArRef = React.createRef();
   }
-  setChallenge = (challengeId)=>{
+  setChallenge = (challengeId) => {
     //do something with your challenge ID.
-  }
-  getToken(token){
-    console.log(
-      token
-    )
+  };
+  getToken(token) {
+    console.log(token);
   }
   render() {
     return (
       <div>
-          <ArcaptchaWidget
-              ref={this.ArRef}
-              site-key=""
-              onsetChallengeId={this.setChallenge}
-              callback={this.getToken}
-              theme='dark' //it's not required. Default is light
-              lang='en' //it's not required. Default is fa
-              />
+        <ArcaptchaWidget
+          ref={this.ArRef}
+          site-key="0k2m92tx9p"
+          onsetChallengeId={this.setChallenge}
+          callback={this.getToken}
+          theme="dark" //it's not required. Default is light
+          lang="en" //it's not required. Default is fa
+        />
+        <div>lfnoruaeg</div>
+        <ArcaptchaWidget
+          ref={this.ArRef}
+          site-key="89oep0u80t"
+          onsetChallengeId={this.setChallenge}
+          callback={this.getToken}
+          theme="dark" //it's not required. Default is light
+          lang="en" //it's not required. Default is fa
+        />
+        <ArcaptchaWidget
+          ref={this.ArRef}
+          site-key="0k2m92tx9p"
+          onsetChallengeId={this.setChallenge}
+          callback={this.getToken}
+          theme="dark" //it's not required. Default is light
+          lang="en" //it's not required. Default is fa
+        />
       </div>
     );
   }
--- src/lib/components/ArcaptchaWidget.js
+++ src/lib/components/ArcaptchaWidget.js
@@ -1,76 +1,81 @@
 import React, { Component } from "react";
 
 class ArCaptcha extends Component {
-    constructor(props) {
-        super(props);
-        this.setRef = () => {
-            return "arcaptcha"
-        };
+  constructor(props) {
+    super(props);
+    this.setRef = () => {
+      return "arcaptcha";
+    };
 
-        // API Methods
-        this.execute = this.execute.bind(this);
-        this.resetCaptcha = this.resetCaptcha.bind(this);
-    }
-    state = {
-        widget_id: "",
-        id: "",
-    }
+    // API Methods
+    this.execute = this.execute.bind(this);
+    this.resetCaptcha = this.resetCaptcha.bind(this);
+  }
+  state = {
+    widget_id: "",
+    id: "",
+  };
 
-    componentDidMount() { //Once captcha is mounted intialize ArCaptcha
-        this.setID()
-        const script = document.createElement("script");
-        script.src = `https://widget.arcaptcha.ir/1/api.js`;
-        script.id = "arcptcha-script"
-        script.onload = () => {
-            this.loadCaptcha()
-            window.addEventListener(
-                `arcaptcha-token-changed-${this.state.widget_id}`,
-                (event, v) => {
-                    this.props.onsetChallengeId(event.detail);
-                }
-            );
+  componentDidMount() {
+    //Once captcha is mounted intialize ArCaptcha
+    this.setID();
+    var my_script = document.head.querySelector("#arcptcha-script");
+    let script = my_script || document.createElement("script");
+    script.src = `https://widget.arcaptcha.ir/1/api.js`;
+    script.id = "arcptcha-script";
+    this.initialize(script);
+    if (!my_script)
+      document.head.appendChild(script);
+  }
+
+  initialize(script) {
+    script.onload = () => {
+      this.loadCaptcha();
+      window.addEventListener(
+        `arcaptcha-token-changed-${this.state.widget_id}`,
+        (event, v) => {
+          this.props.onsetChallengeId(event.detail);
         }
-        if(!document.head.querySelector("#arcptcha-script"))
-            document.head.appendChild(script);
-    }
+      );
+    };
+  }
+  getRandomID() {
+    return (
+      new Date().getTime().toString(36) + Math.random().toString(36).slice(2)
+    );
+  }
 
-    getRandomID() {
-        return (
-            new Date().getTime().toString(36) +
-            Math.random().toString(36).slice(2)
-        );
-    }
+  execute() {
+    window.arcaptcha.execute(this.state.widget_id);
+  }
 
-    execute() {
-        window.arcaptcha.execute(this.state.widget_id);
-    }
+  loadCaptcha() {
+    if (this.props.callback)
+      window[`arcaptcha_callback_${this.state.id}`] = this.props.callback;
 
-    loadCaptcha() {
-        if (this.props.callback)
-            window[`arcaptcha_callback_${this.state.id}`] = this.props.callback;
+    const widgetId = window.arcaptcha.render(`#${this.state.id}`, {
+      "site-key": this.props["site-key"],
+      size: this.props.invisible ? "invisible" : "",
+      callback: this.props.callback
+        ? `arcaptcha_callback_${this.state.id}`
+        : null,
+      lang: this.props.lang,
+      theme: this.props.theme,
+    });
+    this.setState({ widget_id: widgetId });
+  }
 
-        const widgetId = window.arcaptcha.render(`#${this.state.id}`, {
-            "site-key": this.props['site-key'],
-            size: this.props.invisible ? "invisible" : "",
-            callback:this.props.callback ? `arcaptcha_callback_${this.state.id}` : null,
-            lang: this.props.lang,
-            theme: this.props.theme
-        })
+  resetCaptcha() {
+    window.arcaptcha.reset(this.state.widget_id);
+  }
 
-        this.setState({ widget_id: widgetId })
-    }
+  setID() {
+    this.setState({ id: `arcaptcha-widget-${this.getRandomID()}` });
+  }
 
-    resetCaptcha() {
-        window.arcaptcha.reset(this.state.widget_id);
-    }
-
-    setID() {
-        this.setState({ id: `arcaptcha-widget-${this.getRandomID()}` });
-    }
-
-    render() {
-        const elementId = this.state.id;
-        return <div ref={this.setRef} id={elementId}></div>
-    }
+  render() {
+    const elementId = this.state.id;
+    return <div ref={this.setRef} id={elementId}></div>;
+  }
 }
 export default ArCaptcha;
